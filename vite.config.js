d0336257ab108b1389abe248672d661bfaec400d
ansari katib <ansarikatib647@gmail.com>
2024-11-12 14:55:55 +0530

    chage in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
+  base: '/Portfolio/', // replace with your actual GitHub repository name
   plugins: [react()],
 })
