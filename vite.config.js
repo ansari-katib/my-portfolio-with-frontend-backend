c158b8f177c0f2fa7a47ee2a4df48bc7e2f0279f
Ansari Katib <162352897+ansari-katib@users.noreply.github.com>
2024-11-12 06:30:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   assetsInclude: ['**/*.svg', '**/*.js'],
   plugins: [react()],
+   server: {
+    open: true, // Automatically open the browser
+  },
 })
