91ff913d32f52deca2854cf9d7e2eef3457735ba
ansari katib <ansarikatib647@gmail.com>
2025-04-25 12:04:55 +0530

    change in contact

--- frontend/src/Routes/Contact.jsx
+++ frontend/src/Routes/Contact.jsx
@@ -34,7 +34,7 @@ function Contact() {
       setFeedback(""); // Reset feedback message
 
       try {
-        const response = await fetch("http://localhost:3000/api/contact", {
+        const response = await fetch(`https://pbackend.up.railway.app/api/contact`, {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(values),
